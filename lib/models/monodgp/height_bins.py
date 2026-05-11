b655b2a5c1697f662a6d1bd3cc9b3c3f291c4d89
OttoJ <ottoesmann@gmail.com>
2026-05-11 13:01:35 +0200

    new bins

--- lib/models/monodgp/height_bins.py
+++ lib/models/monodgp/height_bins.py
@@ -2,6 +2,6 @@ import numpy as np
 import torch
 
 NUM_HEIGHT_BINS = 5
-HEIGHT_BIN_EDGES = np.array([1.330, 1.447, 1.544, 1.715])
-HEIGHT_BIN_CENTERS = np.array([1.272, 1.401, 1.488, 1.614, 1.839])
+HEIGHT_BIN_EDGES = np.array([1.4485, 1.5461, 1.6622, 1.8198])
+HEIGHT_BIN_CENTERS = np.array([1.3949, 1.4933, 1.5941, 1.7307, 1.8905])
 HEIGHT_BIN_CENTERS_TENSOR = torch.tensor(HEIGHT_BIN_CENTERS, dtype=torch.float32)
